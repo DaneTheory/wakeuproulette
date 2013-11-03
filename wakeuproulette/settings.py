0321b580feccfdd339c3d6b1d53cc0b056fdf313
axsauze <axsauze@gmail.com>
2013-11-03 10:20:38 +0000

    Ready for Presentation

--- wakeuproulette/settings.py
+++ wakeuproulette/settings.py
@@ -5,7 +5,8 @@ import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_ROOT =  '/'.join(PROJECT_ROOT.split('/')[0:-1])
 
-WEB_ROOT = "http://127.0.0.1:8000/"
+#WEB_ROOT = "http://127.0.0.1:8000/"
+WEB_ROOT = "http://77f76d7c.ngrok.com/"
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
