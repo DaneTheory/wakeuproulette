13813a88b2862a9bb5cd67c7a27b154e76d7f1d7
Anton Smyrnov <as8g10@gmail.com>
2013-12-06 09:16:47 +0000

    sending email on contact request

--- accounts/ajax.py
+++ accounts/ajax.py
@@ -6,6 +6,8 @@ from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from datetime import datetime
+from wakeup.tools.toolbox import sms_async, send_async_mail
+from wakeuproulette.settings import EMAIL_HOST_USER
 
 #########################################
 ############### CONTACTS ################
@@ -40,6 +42,7 @@ def accept_request(request):
         contact_request.save()
         contact = Contact.objects.create(user=request.user,contact=contact_request.user,status='A')
         response = {'html': render_to_string('layouts/contact.html', {'contact': contact})}
+        send_async_mail("WakeUpRoulette Accepted Request", request.user.username + " has accepted your contact request. To see the list of your contacts, please login to your account at http://wakeuproulette.com/accounts/dashboard/", EMAIL_HOST_USER, [contact_request.user.email])
     except Contact.DoesNotExist:
         response['error'] = True
     return HttpResponse(json.dumps(response), content_type="application/json")
