096d0f1dcca5a788f602f0eca50cc18cbb058b5b
Anton Smyrnov <as8g10@gmail.com>
2013-12-08 15:21:15 +0000

    fix for timezones in contact list

--- wakeup/templatetags/wakeup_tags.py
+++ wakeup/templatetags/wakeup_tags.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+from wakeup.tools.toolbox import local_time as local_time_func
+
 @register.assignment_tag(takes_context=True)
 def liked(context, share):
     user = context['request'].user
@@ -16,3 +18,7 @@ def liked(context, share):
         ret = False
 
     return 1 if ret else 0
+
+@register.simple_tag(takes_context=True)
+def local_time(context, time):
+    return local_time_func(time, context['request']).strftime("%H:%M")
