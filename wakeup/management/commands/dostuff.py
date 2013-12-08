6d2b83457548aeb2a7fb2da7fb7fdf03f67205b0
Alejandro Saucedo <axsauze@gmail.com>
2013-12-08 17:02:59 +0000

    Added working public dashboard!

--- wakeup/management/commands/dostuff.py
+++ wakeup/management/commands/dostuff.py
@@ -1,7 +1,8 @@
 from django.core.management.base import NoArgsCommand
 
+
 from accounts.models import UserProfile
-from wakeup.models import Recording, RecordingShare, RecordingComment, Call
+from wakeup.models import Call
 
 
 
@@ -13,25 +14,12 @@ class Command(NoArgsCommand):
 
         print "doing stuff..."
 
-
-        u = UserProfile.objects.all()[0].user
-
-        calls = Call.objects.filter(user=u)
+        calls = Call.objects.all()
 
         for call in calls:
-            share = RecordingShare()
-            share.call = call
-            share.user = u
-
-            share.body = "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb."
-
-            share.save()
-
-            comment = RecordingComment()
-            comment.comment = "NO!"
-            comment.recordingshare = share
-            comment.user = u
-            comment.save()
+            if call.recording:
+                call.recording.privacy = 'P'
+                call.recording.save()
 
 
         print "Done doing stuff."
